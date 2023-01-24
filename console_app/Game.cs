3c2d945ff7ee0ffc1b3d26e3fbbbf860338d06c1
Firzus <92395888+Firzus@users.noreply.github.com>
2023-01-24 15:40:04 +0100

    map + setting window

---
+++ console_app/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using console_app;
+
+namespace YOURNAMESPACE
+{
+    [SupportedOSPlatform("windows")]
+    internal static class Game
+    {
+        static void Main()
+        {
+            ConsoleManager.WindowSetup();
+            Map.drawMap();
+        }
+    }
+}
---
+++ console_app/Map.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    internal class Map
+    {
+        public static void drawMap()
+        {
+            // modeling map
+            string[] map =
+            {
+            "#############################################",
+            "#.......#############################.......#",
+            "#.#.#.#.#############################.......#",
+            "#.......#############################.......#",
+            "#############################################"
+            };
+
+            // display map
+            for (int y = 0; y < map.Length; y++)
+            {
+                for (int x = 0; x < map[y].Length; x++)
+                {
+                    char title = map[y][x];
+                    Console.Write(title);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
