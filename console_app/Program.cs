25b62834baf1de9b39c8002c15b687244da8fd74
Firzus <92395888+Firzus@users.noreply.github.com>
2023-01-24 14:37:32 +0100

    first map uwu

--- console_app/Program.cs
+++ console_app/Program.cs
@@ -1,8 +1,32 @@
 ﻿namespace console_app;
+using System;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Fuck the World!");
+        // type de terrain
+        const char WALL = '#';
+        const char FLOOR = '.';
+
+        // modeling map
+        string[] map =
+        {
+            "#########",
+            "#.......#",
+            "#.#.#.#.#",
+            "#.......#",
+            "#########"
+        };
+
+        // afficher la carte
+        for (int y = 0; y < map.Length; y++)
+        {
+            for (int x = 0; x < map.Length; x++)
+            {
+                char title = map[y][x];
+                Console.Write(title);
+            }
+            Console.WriteLine();
+        }
     }
 }
