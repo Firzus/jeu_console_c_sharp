85087957f43ae962c79f2078525f3fed49c43af7
ataillat <ataillat@gaming.tech>
2023-01-25 15:57:49 +0100

    pour merge

---
+++ console_app/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace console_app;
+class Game
+{
+    static void Main(string[] args)
+    {
+        Intro intro= new Intro();
+        intro.drawIntro();
+
+        Menu.menu();
+
+        Console.WriteLine("Fuck the World!");
+    }
+}
