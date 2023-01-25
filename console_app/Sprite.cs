82d2a93aebf16690a709851d75a8387d1f15edba
Kévin <kevin.terrisson@gmail.com>
2023-01-25 16:59:28 +0100

    symétrie v1

--- console_app/Sprite.cs
+++ console_app/Sprite.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace console_app
 {
     internal class Sprite
     {
-        int[,] array = new int[4, 2];
         public static void Spriteextraction()
         {
-            //extraction d'un sprite a partir d'un fichier TXT affin de l'afficcher dans la consol, le string recois le txt et permet donc de le retouner en console
-            string text = System.IO.File.ReadAllText(@"X:\jeu_console_c_sharp\console_app\Sprite.txt");
-            System.Console.WriteLine(text);
+            /*extraction d'un sprite a partir d'un fichier TXT affin de l'afficcher dans la consol, le string recois le txt et permet donc de le retouner en console
+             pour avoir une extraction d'autre fichier il suffira de copier la ligne ci dessoue et de mettre le bon path*/
+            string[] text = System.IO.File.ReadAllLines(@"X:\jeu_console_c_sharp\console_app\Sprite\Sprite.txt");
+            
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i >= 0)
+                {
+                    //text[i] = i.ToString("2");
+                    var el = text[i];
+                    text[i] = el.Reverse().Select(i => i.ToString()).Aggregate((a, b) => a + b);
+                    //text[i] = el.Replace('\', '0');
+                }
+            }
+
+            foreach (var el  in text)
+            {
+                Console.WriteLine(el);
+            }
+            System.Console.Write(text);
+
+            /*StringBuilder sb = new StringBuilder(text);
+            string reversed = new string(text.Reverse().ToArray()); // retourne le sprite (lui met la tete en bas)
+
+            Console.WriteLine(reversed);*/
+            
+            
+
 
             Console.WriteLine("Press any key to exit."); 
             System.Console.ReadKey();
         }
-
     }
 }
