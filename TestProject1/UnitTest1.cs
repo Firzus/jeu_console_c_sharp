4bae1d6658ee591e0f66aa43f1ddaaec1a06f84f
Kévin <75180055+KevinTerrisson@users.noreply.github.com>
2023-01-26 09:36:58 +0100

    premier test unitaire

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using console_app;
+using System.Net.Http.Headers;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
