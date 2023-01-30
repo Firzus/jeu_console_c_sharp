358579d52e53b9984a33cfade0845991c043d384
Kévin <kevin.terrisson@gmail.com>
2023-01-30 12:02:33 +0100

    map

--- console_app/Map.cs
+++ console_app/Map.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace console_app
 {
@@ -10,43 +12,47 @@ namespace console_app
     {
         public static void DrawMap()
         {
-            // modeling map
-            string[] map =
-            {
-                "   ┌─────────────────────────────────────┐",
-                "   │#####################################│",
-                "   │##############     ############### ##│",
-                "   │############    !  %%%%  #####  #   #│",
-                "   │###########        %%%%             @│",
-                "   │#######            %%%%              │",
-                "   │#####              ####      ##### ##│",
-                "   │@  ###          ########     ########│",
-                "   │#              ##########      ######│",
-                "   │#              ##############     ###│",
-                "   │###           ####%%%#########    &&#│",
-                "   │####              %%%##########   &&#│",
-                "   │#############       ##############&&#│",
-                "   │##############     ##################│",
-                "   │#####################################│",
-                "   └─────────────────────────────────────┘"
-            };
 
-            // modeling character
-            string character = "?";
+            string[] map = System.IO.File.ReadAllLines(@"X:\jeu_console_c_sharp\console_app\Sprite\Map.txt");
+            System.Console.Write(map);
 
-            // modeling zap
-            string zap = "*";
+            /* // modeling map
+             string[] map =
+             {
+                 "   ┌─────────────────────────────────────┐",
+                 "   │#####################################│",
+                 "   │##############     ############### ##│",
+                 "   │############    !  %%%%  #####  #   #│",
+                 "   │###########        %%%%             @│",
+                 "   │#######            %%%%              │",
+                 "   │#####              ####      ##### ##│",
+                 "   │@  ###          ########     ########│",
+                 "   │#              ##########      ######│",
+                 "   │#              ##############     ###│",
+                 "   │###           ####%%%#########    &&#│",
+                 "   │####              %%%##########   &&#│",
+                 "   │#############       ##############&&#│",
+                 "   │##############     ##################│",
+                 "   │#####################################│",
+                 "   └─────────────────────────────────────┘"    
+             };
 
-            // display map
-            for (int y = 0; y < map.Length; y++)
-            {
-                for (int x = 0; x < map[y].Length; x++)
-                {
-                    char title = map[y][x];
-                    Console.Write(title);
-                }
-                Console.WriteLine();
-            }
+             // modeling character
+             string character = "?";
+
+             // modeling zap
+             string zap = "*";
+
+             // display map
+             for (int y = 0; y < map.Length; y++)
+             {
+                 for (int x = 0; x < map[y].Length; x++)
+                 {
+                     char title = map[y][x];
+                     Console.Write(title);
+                 }
+                 Console.WriteLine();
+        }*/
         }
     }
 }
