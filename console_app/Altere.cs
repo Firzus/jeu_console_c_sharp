987c6fd200db76961bfc52cee3d34080a811cf3e
Firzus <92395888+Firzus@users.noreply.github.com>
2023-02-01 23:15:17 +0100

    add content

---
+++ console_app/Altere.cs
@@ -0,0 +1,27 @@
+﻿using console_app.Pokemons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    internal class Altere
+    {
+        public void TakeDamage(float nbDegats)
+        {
+            m_vie -= nbDegats;
+
+            if (m_vie < 0)
+            {
+                m_vie = 0;
+            }
+        }
+
+        public void Attack(Altere cible)
+        {
+            cible.TakeDamage((1 - m_defence / 100) * m_degats);
+        }
+    }
+}
---
+++ console_app/Altere/Pikachu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using console_app.Objects;
+
+namespace console_app.Pokemons
+{
+    internal class Pikachu
+    {
+        static private string m_name = "Pikachu";
+        private float m_life = 50.0f;
+        private float m_damage = 20.0f;
+        static private float m_speed = 30.0f;
+        // varie de 0 à 100 , 50 = 50% de res
+        private float m_defence = 0.0f;
+
+        static public Pikachu Create()
+        {
+            return new Pikachu();
+        }
+
+        public float GetLife()
+        {
+            return m_life;
+        }
+
+        public float GetDamage()
+        {
+            return m_damage;
+        }
+
+        public bool IsAlive()
+        {
+            return m_life > 0;
+        }
+
+        static public float GetSpeed()
+        {
+            return m_speed;
+        }
+
+        static public string GetName()
+        {
+            return m_name;
+        }
+
+        /*DEBUG*/
+
+        public void Infos()
+        {
+            Console.WriteLine("Vie Pikachu: " + m_life);
+        }
+
+        /*DEBUG*/
+    }
+}
